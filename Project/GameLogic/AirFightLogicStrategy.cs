a92126f58307c870a9b7ff3a86332127ad6b755d
Zsebe Akos <zsebea@yahoo.com>
2018-01-11 15:16:21 +0200

    added code map + fixed solder - to soldier

--- Project/GameLogic/AirFightLogicStrategy.cs
+++ Project/GameLogic/AirFightLogicStrategy.cs
@@ -5,23 +5,23 @@ namespace Project.GameLogic
 {
     class AirFightLogicStrategy : IGameLogicStrategy
     {
-        public void Fight(ref SoldierBehavior solder1,ref SoldierBehavior solder2)
+        public void Fight(ref SoldierBehavior soldier1,ref SoldierBehavior soldier2)
         {
             int s1x = 5;
             int s2x = 5;
-            if (solder1.GetName() == "GroundforcesSoldierBehavior") s1x = 1;
-            if (solder2.GetName() == "GroundforcesSoldierBehavior") s2x = 1;
-            if (solder1.GetName() == "NavyforceSoldierBehavior") s1x = 5;
-            if (solder2.GetName() == "NavyforceSoldierBehavior") s2x = 5;
+            if (soldier1.GetName() == "GroundforcesSoldierBehavior") s1x = 1;
+            if (soldier2.GetName() == "GroundforcesSoldierBehavior") s2x = 1;
+            if (soldier1.GetName() == "NavyforceSoldierBehavior") s1x = 5;
+            if (soldier2.GetName() == "NavyforceSoldierBehavior") s2x = 5;
 
             var whoShoots = new Random();
             if (whoShoots.Next(1, 100) % 2 == 0)
             {
-                solder1.Attak(ref solder2, s1x);
+                soldier1.Attak(ref soldier2, s1x);
             }
             else
             {
-                solder2.Attak(ref solder1, s2x);
+                soldier2.Attak(ref soldier1, s2x);
             }
 
         }
--- Project/GameLogic/GroundFightLogicStrategy.cs
+++ Project/GameLogic/GroundFightLogicStrategy.cs
@@ -5,23 +5,23 @@ namespace Project.GameLogic
 {
     class GroundFightLogicStrategy : IGameLogicStrategy
     {
-        public void Fight(ref SoldierBehavior solder1,ref SoldierBehavior solder2)
+        public void Fight(ref SoldierBehavior soldier1,ref SoldierBehavior soldier2)
         {
             int s1x = 1;
             int s2x = 1;
-            if (solder1.GetName() == "AirforceSoldierBehavior") s1x = 5;
-            if (solder2.GetName() == "AirforceSoldierBehavior") s2x = 5;
-            if (solder1.GetName() == "NavyforceSoldierBehavior") s1x = 0;
-            if (solder2.GetName() == "NavyforceSoldierBehavior") s2x = 0;
+            if (soldier1.GetName() == "AirforceSoldierBehavior") s1x = 5;
+            if (soldier2.GetName() == "AirforceSoldierBehavior") s2x = 5;
+            if (soldier1.GetName() == "NavyforceSoldierBehavior") s1x = 0;
+            if (soldier2.GetName() == "NavyforceSoldierBehavior") s2x = 0;
 
             var whoShoots = new Random();
             if (whoShoots.Next(1, 100) % 2 == 0)
             {
-                solder1.Attak(ref solder2, s1x);
+                soldier1.Attak(ref soldier2, s1x);
             }
             else
             {
-                solder2.Attak(ref solder1, s2x);
+                soldier2.Attak(ref soldier1, s2x);
             }
 
         }
